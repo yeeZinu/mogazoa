02dc08dc62e27e7f068bb141fb0c0e228d9d410b
aoooec <foottable@gmail.com>
2024-05-22 21:50:02 +0900

    chore: 이미지 assets 및 상수 파일 추가

---
+++ src/utils/constant.ts
@@ -0,0 +1,51 @@
+const ADD_ICON = "/images/add.svg";
+const BUBBLE_ICON = "/images/bubble.svg";
+const CATEGORY_ICON = "/images/category.svg";
+const CLOSE_ICON = "/images/close.svg";
+const DROP_DOWN_ICON = "/images/drop-down.svg";
+const DROP_UP_ICON = "/images/drop-up.svg";
+const MENU_TOGGLE_ICON = "/images/menu.svg";
+const MY_PROFILE_ICON = "/images/my-profile.svg";
+const PHOTO_ICON = "/images/photo.svg";
+const SAVE_ICON = "/images/save.svg";
+const UNSAVE_ICON = "/images/unsave.svg";
+const SEARCH_ICON = "/images/search.svg";
+const SHARE_ICON = "/images/search.svg";
+const GOOGLE_ICON = "/images/sns-google.svg";
+const KAKAO_ICON = "/images/sns-kakao.svg";
+const STAR_ACTIVE_ICON = "/images/star.svg";
+const STAR_ICON = "/images/star-gray.svg";
+const THUMBS_UP_ICON = "/images/thumbs-up-inactive.svg";
+const THUMBS_UP_ACTIVE_ICON = "/images/thumbs-up-active.svg";
+const EYE_OFF_ICON = "/images/visibility-off.svg";
+const EYE_ON_ICON = "/images/visibility-on.svg";
+const LOADING_SMALL_IMAGE = "/images/loading-small.svg";
+const LOADING_LARGE_IMAGE = "/images/loading-large.svg";
+const LOGO_IMAGE = "/images/main-logo.svg";
+
+export {
+  ADD_ICON,
+  BUBBLE_ICON,
+  CATEGORY_ICON,
+  CLOSE_ICON,
+  DROP_DOWN_ICON,
+  DROP_UP_ICON,
+  MENU_TOGGLE_ICON,
+  MY_PROFILE_ICON,
+  PHOTO_ICON,
+  SAVE_ICON,
+  UNSAVE_ICON,
+  SEARCH_ICON,
+  SHARE_ICON,
+  GOOGLE_ICON,
+  KAKAO_ICON,
+  STAR_ICON,
+  STAR_ACTIVE_ICON,
+  THUMBS_UP_ICON,
+  THUMBS_UP_ACTIVE_ICON,
+  EYE_OFF_ICON,
+  EYE_ON_ICON,
+  LOADING_LARGE_IMAGE,
+  LOADING_SMALL_IMAGE,
+  LOGO_IMAGE,
+};
