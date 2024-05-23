9945894551601f675d295426f735dcc06fcdf6ae
zoonyoung <9851248@gmail.com>
2024-05-23 13:20:05 +0900

    fix: export 방법 변경 (export {a, b,c} -> export const = '')

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,51 +1,24 @@
-const ADD_ICON = "/images/add.svg";
-const BUBBLE_ICON = "/images/bubble.svg";
-const CATEGORY_ICON = "/images/category.svg";
-const CLOSE_ICON = "/images/close.svg";
-const DROP_DOWN_ICON = "/images/drop-down.svg";
-const DROP_UP_ICON = "/images/drop-up.svg";
-const MENU_TOGGLE_ICON = "/images/menu.svg";
-const MY_PROFILE_ICON = "/images/my-profile.svg";
-const PHOTO_ICON = "/images/photo.svg";
-const SAVE_ICON = "/images/save.svg";
-const UNSAVE_ICON = "/images/unsave.svg";
-const SEARCH_ICON = "/images/search.svg";
-const SHARE_ICON = "/images/search.svg";
-const GOOGLE_ICON = "/images/sns-google.svg";
-const KAKAO_ICON = "/images/sns-kakao.svg";
-const STAR_ACTIVE_ICON = "/images/star.svg";
-const STAR_ICON = "/images/star-gray.svg";
-const THUMBS_UP_ICON = "/images/thumbs-up-inactive.svg";
-const THUMBS_UP_ACTIVE_ICON = "/images/thumbs-up-active.svg";
-const EYE_OFF_ICON = "/images/visibility-off.svg";
-const EYE_ON_ICON = "/images/visibility-on.svg";
-const LOADING_SMALL_IMAGE = "/images/loading-small.svg";
-const LOADING_LARGE_IMAGE = "/images/loading-large.svg";
-const LOGO_IMAGE = "/images/main-logo.svg";
-
-export {
-  ADD_ICON,
-  BUBBLE_ICON,
-  CATEGORY_ICON,
-  CLOSE_ICON,
-  DROP_DOWN_ICON,
-  DROP_UP_ICON,
-  MENU_TOGGLE_ICON,
-  MY_PROFILE_ICON,
-  PHOTO_ICON,
-  SAVE_ICON,
-  UNSAVE_ICON,
-  SEARCH_ICON,
-  SHARE_ICON,
-  GOOGLE_ICON,
-  KAKAO_ICON,
-  STAR_ICON,
-  STAR_ACTIVE_ICON,
-  THUMBS_UP_ICON,
-  THUMBS_UP_ACTIVE_ICON,
-  EYE_OFF_ICON,
-  EYE_ON_ICON,
-  LOADING_LARGE_IMAGE,
-  LOADING_SMALL_IMAGE,
-  LOGO_IMAGE,
-};
+export const ADD_ICON = "/images/add.svg";
+export const BUBBLE_ICON = "/images/bubble.svg";
+export const CATEGORY_ICON = "/images/category.svg";
+export const CLOSE_ICON = "/images/close.svg";
+export const DROP_DOWN_ICON = "/images/drop-down.svg";
+export const DROP_UP_ICON = "/images/drop-up.svg";
+export const MENU_TOGGLE_ICON = "/images/menu.svg";
+export const MY_PROFILE_ICON = "/images/my-profile.svg";
+export const PHOTO_ICON = "/images/photo.svg";
+export const SAVE_ICON = "/images/save.svg";
+export const UNSAVE_ICON = "/images/unsave.svg";
+export const SEARCH_ICON = "/images/search.svg";
+export const SHARE_ICON = "/images/search.svg";
+export const GOOGLE_ICON = "/images/sns-google.svg";
+export const KAKAO_ICON = "/images/sns-kakao.svg";
+export const STAR_ACTIVE_ICON = "/images/star.svg";
+export const STAR_ICON = "/images/star-gray.svg";
+export const THUMBS_UP_ICON = "/images/thumbs-up-inactive.svg";
+export const THUMBS_UP_ACTIVE_ICON = "/images/thumbs-up-active.svg";
+export const EYE_OFF_ICON = "/images/visibility-off.svg";
+export const EYE_ON_ICON = "/images/visibility-on.svg";
+export const LOADING_SMALL_IMAGE = "/images/loading-small.svg";
+export const LOADING_LARGE_IMAGE = "/images/loading-large.svg";
+export const LOGO_IMAGE = "/images/main-logo.svg";
