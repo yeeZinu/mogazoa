502c6e0382e2727584d95d1ece112721a85bebe4
suzinxix <suzinxix@gmail.com>
2024-05-25 00:40:05 +0900

    feat: 상품 카드 컴포넌트 구현

---
+++ src/components/Product/Product.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { STAR_ACTIVE_ICON, DEFAULT_PROFILE_IMAGE } from "@/utils/constant";
+import styles from "./Product.module.scss";
+
+type ProductProps = {
+  product: {
+    name: string;
+    reviewCount: number;
+    favoriteCount: number;
+    rating: number;
+    image: string;
+  };
+};
+
+export default function Product({ product }: ProductProps) {
+  const { name, reviewCount, favoriteCount, rating, image: imageUrl } = product;
+
+  const [isImageError, setIsImageError] = useState(false);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageBox}>
+        <Image
+          src={isImageError ? DEFAULT_PROFILE_IMAGE : imageUrl}
+          fill
+          alt={`${name} 이미지`}
+          className={styles.image}
+          onError={() => setIsImageError(true)}
+        />
+      </div>
+
+      <div className={styles.infoBox}>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.infoDetailBox}>
+          <div className={styles.reviewFavorite}>
+            <span>리뷰 {reviewCount}</span>
+            <span>찜 {favoriteCount}</span>
+          </div>
+
+          <div className={styles.ratingBox}>
+            <div className={styles.icon}>
+              <Image
+                src={STAR_ACTIVE_ICON}
+                fill
+                alt='별 아이콘'
+              />
+            </div>
+            <span className={styles.rating}>{rating}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
