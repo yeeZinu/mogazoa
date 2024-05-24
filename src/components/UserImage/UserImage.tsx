43fa9158d4c5f9618a4c69444537e15d2db520fd
aoooec <foottable@gmail.com>
2024-05-24 16:54:29 +0900

    fix: 잘못된 url일 경우 기본 이미지 노출, 프로필 이미지 사이즈 최적화

--- src/components/UserImage/UserImage.tsx
+++ src/components/UserImage/UserImage.tsx
@@ -1,16 +1,21 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 import cn from "@/utils/classNames";
 import { DEFAULT_PROFILE_IMAGE } from "@/utils/constant";
 import styles from "./UserImage.module.scss";
 
 type UserImageProps = {
   /* eslint-disable-next-line react/require-default-props */
-  image?: string;
+  image: string;
   nickname: string;
   size: "small" | "medium" | "large";
 };
 
-export default function UserImage({ image = DEFAULT_PROFILE_IMAGE, nickname, size }: UserImageProps) {
+export default function UserImage({ image, nickname, size }: UserImageProps) {
+  const [isError, setError] = useState(false);
+
   return (
     <div
       className={cn(
@@ -23,7 +28,10 @@ export default function UserImage({ image = DEFAULT_PROFILE_IMAGE, nickname, siz
       <Image
         fill
         alt={`${nickname}'s profile image`}
-        src={image}
+        src={isError || !image ? DEFAULT_PROFILE_IMAGE : image}
+        onError={() => setError(true)}
+        sizes='(max-width: 768px) 300px, 500px'
+        className={cn(styles.image)}
       />
     </div>
   );
