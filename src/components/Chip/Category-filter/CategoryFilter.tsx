90495b6d31087b9480e9ebd90dcec8c061a22a31
yubin <yb3143@gmail.com>
2024-05-25 13:32:39 +0900

    feat: add category chip

--- src/components/Chip/Category-filter/CategoryFilter.tsx
+++ src/components/Chip/Category-filter/CategoryFilter.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
 import React from "react";
+import categoryImg from "@/images/category.svg";
+import styles from "./CategoryFilter.module.scss";
 
-function CategoryFilter() {
-  return <div>CategoryFilter</div>;
+type CategoryFilterProps = {
+  children: React.ReactNode;
+};
+
+function CategoryFilter({ children }: CategoryFilterProps) {
+  return (
+    <div className={styles.container}>
+      <Image
+        src={categoryImg}
+        alt='category'
+        width={18}
+        height={18}
+      />
+      {children}
+    </div>
+  );
 }
 
 export default CategoryFilter;
