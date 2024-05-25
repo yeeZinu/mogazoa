dc6ea8cb0a8fbb7371fc1a0e92b9d7cc33aa11e2
yubin <yb3143@gmail.com>
2024-05-25 12:30:39 +0900

    feat: add ranking

---
+++ src/components/Chip/Category-filter/CategoryFilter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CategoryFilter() {
+  return <div>CategoryFilter</div>;
+}
+
+export default CategoryFilter;
---
+++ src/components/Chip/Ranking/Ranking.tsx
@@ -0,0 +1,36 @@
+import React, { HTMLAttributes } from "react";
+import styles from "./Ranking.module.scss";
+
+type RankingProps = {
+  children?: keyof typeof RANKING_TYPE;
+} & HTMLAttributes<HTMLDivElement>;
+
+type RankingType<T extends string> = {
+  [key in T]: { style: string };
+};
+
+const RANKING_TYPE: RankingType<string> = {
+  1: { style: "first" },
+  2: { style: "second" },
+  3: { style: "third" },
+  default: { style: "default" },
+};
+
+function Ranking({ children, ...rest }: RankingProps) {
+  const typeCheck = children ?? "default";
+
+  return (
+    <div
+      className={`${styles[RANKING_TYPE[typeCheck]?.style] ?? styles.default}`}
+      {...rest}
+    >
+      <div>{children}등</div>
+    </div>
+  );
+}
+
+Ranking.defaultProps = {
+  children: "default",
+};
+
+export default Ranking;
