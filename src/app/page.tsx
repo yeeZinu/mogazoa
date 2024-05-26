3010ee4983f110e3295a5a851c8dba8105ea4ca3
yeeZinu <doglife222@gmail.com>
2024-05-27 08:25:10 +0900

    fix: page 원상복구 (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-import Button from "@/components/Button/Button";
-
 export default function Home() {
-  return (
-    <div>
-      <Button
-        styleType='tertiary'
-        disabled
-      >
-        이게 닫히는 버튼임 ㅇㅇ
-      </Button>
-    </div>
-  );
+  return <div>Hello</div>;
 }
