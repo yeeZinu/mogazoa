d12fe242e992ce83d6b7db0e7afcfc23f5085f3d
yubin <yb3143@gmail.com>
2024-05-29 13:55:38 +0900

    [#28] feat: comparePage 레이아웃

--- src/app/compare/page.tsx
+++ src/app/compare/page.tsx
@@ -4,7 +4,28 @@ import styles from "./compare.module.scss";
 function ComparePage() {
   return (
     <div className={styles.background}>
-      <div>hi</div>
+      <div className={styles.content}>
+        <div>
+          <div className={styles.text}>상품 1</div>
+          <input
+            type='text'
+            className={styles.input}
+          />
+        </div>
+        <div>
+          <div className={styles.text}>상품 2</div>
+          <input
+            type='text'
+            className={styles.input}
+          />
+        </div>
+        <button
+          type='button'
+          className={styles.button}
+        >
+          비교하기
+        </button>
+      </div>
     </div>
   );
 }
