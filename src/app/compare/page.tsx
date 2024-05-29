d92963aa5dc596fc7ed7d37bb4e60bbe70583d90
yubin <yb3143@gmail.com>
2024-05-29 11:19:01 +0900

    [#28] feat: comparePage 생성

---
+++ src/app/compare/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./compare.module.scss";
+
+function ComparePage() {
+  return (
+    <div className={styles.background}>
+      <div>hi</div>
+    </div>
+  );
+}
+
+export default ComparePage;
