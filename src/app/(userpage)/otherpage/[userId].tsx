aa9e1a42c45e9811f5666e708161bcf426b92f29
yeeZinu <doglife222@gmail.com>
2024-05-31 21:15:12 +0900

    feat: otherpage dynamic routing 으로 변경 (#35)

--- src/app/(userpage)/otherpage/[userId].tsx
+++ src/app/(userpage)/otherpage/[userId].tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import cn from "@/utils/classNames";
-import styles from "./MyPage.module.scss";
+import styles from "./OtherPage.module.scss";
 
 export default function OtherPage() {
   return <div className={cn(styles.container)}>OtherPage</div>;
