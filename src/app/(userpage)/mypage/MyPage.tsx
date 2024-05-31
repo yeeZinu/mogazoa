8cf30ed9e72c52f7d07712bff8532c80eb634851
yeeZinu <doglife222@gmail.com>
2024-05-31 21:03:25 +0900

    feat: 폴더 기본구조 세팅 (#35)

---
+++ src/app/(userpage)/mypage/MyPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import cn from "@/utils/classNames";
+import styles from "./MyPage.module.scss";
+
+export default function MyPage() {
+  return <div className={cn(styles.container)}>MyPage</div>;
+}
