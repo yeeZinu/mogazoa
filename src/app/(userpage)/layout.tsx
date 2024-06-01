1e0fb562d9fcf384a12462dba95bd99fa6060d28
yeeZinu <doglife222@gmail.com>
2024-06-01 17:10:08 +0900

    style: layout에 background 추가 (#35)

--- src/app/(userpage)/layout.tsx
+++ src/app/(userpage)/layout.tsx
@@ -1,3 +1,5 @@
+import styles from "./layout.module.scss";
+
 export default function UserLayout({ children }: { children: React.ReactNode }) {
-  return <section>{children}</section>;
+  return <section className={styles.container}>{children}</section>;
 }
