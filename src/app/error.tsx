61edb0f01d68f4f35ff0fc2595789b908fd23735
suzinxix <suzinxix@gmail.com>
2024-06-07 11:46:31 +0900

    style: 주석 제거

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,24 +1,16 @@
 "use client";
 
-// Error components must be Client Components
-
 import { useEffect } from "react";
 
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
+  useEffect(() => {}, [error]);
 
   return (
     <div>
       <h2>에러가 발생하였습니다. 다시 시도해 주세요</h2>
       <button
         type='button'
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
+        onClick={() => reset()}
       >
         Try again
       </button>
