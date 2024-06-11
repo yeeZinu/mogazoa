761254ce07d2af3e4869203f4d015ed2ac7abb3a
zoonyoung <9851248@gmail.com>
2024-06-11 12:59:57 +0900

    feat: httpClient 클래스 구현

---
+++ src/utils/httpClient.ts
@@ -0,0 +1,44 @@
+export default class HttpClient {
+  baseUrl: string;
+
+  constructor(baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
+
+  private async sendRequest<T, U>(path: string, method: string, body?: U, options: RequestInit = {}): Promise<T> {
+    try {
+      const requestOptions: RequestInit = {
+        method,
+        headers: { "Content-Type": "application/json", ...options.headers },
+        ...options,
+      };
+      if (body) {
+        requestOptions.body = JSON.stringify(body);
+      }
+      const response = await fetch(`${this.baseUrl}/${path}`, requestOptions);
+      if (!response.ok) {
+        throw new Error(`HTTP error ${response.status}`);
+      }
+      return await response.json();
+    } catch (error) {
+      console.log(`fetch data error, ${error}`);
+      throw error;
+    }
+  }
+
+  async get<T>(path: string, options: RequestInit = {}): Promise<T> {
+    return this.sendRequest<T, never>(path, "GET", undefined, options);
+  }
+
+  async post<T, U>(path: string, body: U, options: RequestInit = {}): Promise<T> {
+    return this.sendRequest<T, U>(path, "POST", body, options);
+  }
+
+  async put<T, U>(path: string, body: U, options: RequestInit = {}): Promise<T> {
+    return this.sendRequest<T, U>(path, "PUT", body, options);
+  }
+
+  async delete<T>(path: string, options: RequestInit = {}): Promise<T> {
+    return this.sendRequest<T, never>(path, "DELETE", undefined, options);
+  }
+}
