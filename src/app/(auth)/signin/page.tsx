5b33e619c082b268fb68b6aee05508d10baed77f
aoooec <foottable@gmail.com>
2024-06-13 22:20:43 +0900

    fix: fix oauth signin feature in signin page & remove unnecessary code (#27)

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -2,56 +2,54 @@
 
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
-import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 import { GOOGLE_ICON, KAKAO_ICON } from "@/utils/constant";
-import OauthSignInButton from "./OauthSignInButton/OauthSignInButton";
-
-type FormData = {
-  email: string;
-  password: string;
-};
+import OauthSignInButton from "./_components/OauthSignInButton/OauthSignInButton";
 
 export default function SignInPage() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>();
   const router = useRouter();
 
-  const onSubmit = async (data: FormData) => {
-    const result = await signIn("credentials", {
-      redirect: false,
-      email: data.email,
-      password: data.password,
-    });
-
-    if (result?.ok) {
-      router.push("/");
-    } else {
-      console.log("로그인 실패");
+  const handleKakaoSignIn = async () => {
+    // 카카오 SDK 초기화
+    if (!window.Kakao.isInitialized()) {
+      window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
     }
+    // 인가 코드 요청
+    window.Kakao.Auth.authorize({
+      redirectUri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
+      scope: "openid",
+    });
   };
+
+  useEffect(() => {
+    const handleKakaoCallback = async () => {
+      const code = new URL(window.location.href).searchParams.get("code") ?? "";
+
+      if (!localStorage.getItem("authCode") && code) {
+        // 다시 redirect됐을 경우 요청 두 번 방지
+        localStorage.setItem("authCode", code);
+        const result = await signIn("kakao", { redirect: false, callbackUrl: "/", code });
+
+        if (result?.status === 403) {
+          localStorage.removeItem("authCode");
+          router.push("/oauth/signup/kakao");
+        }
+      }
+    };
+    handleKakaoCallback();
+  }, [router]);
 
   return (
     <div>
-      Sign In
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("email", { required: "이메일을 입력해주세요." })} />
-        {errors.email && <p>{errors.email.message}</p>}
-        <input {...register("password", { required: "비밀번호를 입력해주세요." })} />
-        {errors.password && <p>{errors.password.message}</p>}
-        <button type='submit'>submit</button>
-      </form>
       <OauthSignInButton
         image={GOOGLE_ICON}
         name='google'
-        onClick={() => signIn("google")}
+        onClick={() => signIn("google", { callbackUrl: "/" })}
       />
       <OauthSignInButton
         image={KAKAO_ICON}
         name='kakao'
-        onClick={() => signIn("kakao")}
+        onClick={handleKakaoSignIn}
       />
     </div>
   );
