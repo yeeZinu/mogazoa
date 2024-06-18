b239e0a2635cfb8c7f1fedc3d4133ed9a2cfc803
aoooec <foottable@gmail.com>
2024-06-18 17:07:30 +0900

    feat: 간편회원가입폼 에러처리 추가(#27)

--- src/app/(auth)/oauth/signup/[provider]/_components/OauthSignUpForm/OauthSignUpForm.tsx
+++ src/app/(auth)/oauth/signup/[provider]/_components/OauthSignUpForm/OauthSignUpForm.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { signIn } from "next-auth/react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { AlertModal } from "@/app/(auth)/_components/AlertModal";
 import { LabelBox } from "@/app/(auth)/_components/LabelBox";
 import { SIGNUP_VALIDATION } from "@/app/(auth)/constants";
 import Button from "@/components/Button/Button";
@@ -16,12 +20,18 @@ type OauthSignUpFormData = {
 };
 
 export default function OauthSignUpForm({ provider, token }: OauthSignUpFormProps) {
+  const [isModal, setIsModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
   const {
     register,
     handleSubmit,
-    formState: { isValid, errors },
+    formState: { isValid, errors, isSubmitting },
     setError,
   } = useForm<OauthSignUpFormData>({ mode: "onBlur" });
+
+  const handleModalClose = () => {
+    setIsModal(false);
+  };
 
   const onSubmit = async (data: OauthSignUpFormData) => {
     const result = await signIn("easySignup", {
@@ -31,37 +41,48 @@ export default function OauthSignUpForm({ provider, token }: OauthSignUpFormProp
       nickname: data.nickname,
     });
 
-    if (result?.error) {
-      setError("nickname", { message: "error" });
+    if (result?.error && result?.error.includes("닉네임")) {
+      setError("nickname", { message: result.error });
+    } else if (result?.error) {
+      setModalMessage(result.error);
+      setIsModal(true);
     }
   };
 
   return (
-    <form
-      className={styles.container}
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <LabelBox
-        title='닉네임'
-        htmlFor='nickname'
+    <>
+      <AlertModal
+        isModal={isModal}
+        handleClose={handleModalClose}
       >
-        <Input
-          id='nickname'
-          name='nickname'
-          type='text'
-          placeholder='닉네임을 입력해 주세요'
-          register={register}
-          rules={SIGNUP_VALIDATION.nickname}
-          errors={errors}
-        />
-      </LabelBox>
-      <Button
-        className={styles.button}
-        styleType='primary'
-        disabled={!isValid}
+        {modalMessage}
+      </AlertModal>
+      <form
+        className={styles.container}
+        onSubmit={handleSubmit(onSubmit)}
       >
-        가입하기
-      </Button>
-    </form>
+        <LabelBox
+          title='닉네임'
+          htmlFor='nickname'
+        >
+          <Input
+            id='nickname'
+            name='nickname'
+            type='text'
+            placeholder='닉네임을 입력해 주세요'
+            register={register}
+            rules={SIGNUP_VALIDATION.nickname}
+            errors={errors}
+          />
+        </LabelBox>
+        <Button
+          className={styles.button}
+          styleType='primary'
+          disabled={!isValid || isSubmitting}
+        >
+          {isSubmitting ? "가입중..." : "가입하기"}
+        </Button>
+      </form>
+    </>
   );
 }
