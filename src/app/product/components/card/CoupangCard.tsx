40bbe12c3dbf01bda831f6e7f30cadb9c0ee22b8
aoooec <foottable@gmail.com>
2024-06-21 23:01:03 +0900

    design: Shopping 컴포넌트 스타일 수정, 상품명 제한 추가

--- src/app/product/components/card/CoupangCard.tsx
+++ src/app/product/components/card/CoupangCard.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 
+import Image from "next/image";
 import { CoupangProduct } from "@/app/product/utils/types";
+import { COUPANG_LOGO } from "@/utils/constant";
 import styles from "./CoupangCard.module.scss";
 
 export default function CoupnagCard({ product }: { product: CoupangProduct }) {
   const { title, image, price, link, rocketShippingImage, rocketGlobalImage } = product;
+  const formattedTitle = title.length > 45 ? `${title.slice(0, 44)}...` : title;
+
   const rocketImage = rocketShippingImage || rocketGlobalImage || null;
   return (
     <a
@@ -13,18 +17,23 @@ export default function CoupnagCard({ product }: { product: CoupangProduct }) {
       target='_blank'
       rel='noopener noreferrer'
     >
-      <img
-        className={styles.productImage}
-        src={image}
-        alt='product'
-      />
-      <div className={styles.description}>
+      <div className={styles.productImageBox}>
         <img
-          className={styles.coupangImage}
-          src='https://image7.coupangcdn.com/image/coupang/common/logo_coupang_w350.png'
-          alt='coupang'
+          className={styles.productImage}
+          src={image}
+          alt='product'
         />
-        <h2 className={styles.name}>{title}</h2>
+      </div>
+      <div className={styles.description}>
+        <div className={styles.logoBox}>
+          <Image
+            className={styles.coupangImage}
+            src={COUPANG_LOGO}
+            alt='coupang'
+            fill
+          />
+        </div>
+        <h2 className={styles.name}>{formattedTitle}</h2>
         <div className={styles.priceBox}>
           <p className={styles.price}>{price}원</p>
           {rocketImage && (
--- src/app/product/components/card/NaverCard.tsx
+++ src/app/product/components/card/NaverCard.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 
+import Image from "next/image";
 import { NaverProduct } from "@/app/product/utils/types";
+import { NAVER_LOGO } from "@/utils/constant";
 import styles from "./NaverCard.module.scss";
 
 export default function NaverCard({ product }: { product: NaverProduct }) {
   const { title, image, price, link } = product;
+  const formattedTitle = title.length > 45 ? `${title.slice(0, 44)}...` : title;
+
   return (
     <a
       className={styles.container}
@@ -12,14 +16,23 @@ export default function NaverCard({ product }: { product: NaverProduct }) {
       target='_blank'
       rel='noopener noreferrer'
     >
-      <img
-        className={styles.productImage}
-        src={image}
-        alt='product'
-      />
+      <div className={styles.productImageBox}>
+        <img
+          className={styles.productImage}
+          src={image}
+          alt='product'
+        />
+      </div>
       <div className={styles.description}>
-        <p className={styles.naver}>NAVER</p>
-        <h2 className={styles.name}>{title}</h2>
+        <div className={styles.logoBox}>
+          <Image
+            className={styles.naver}
+            src={NAVER_LOGO}
+            alt='naver'
+            fill
+          />
+        </div>
+        <h2 className={styles.name}>{formattedTitle}</h2>
         <p className={styles.price}>{price}원</p>
       </div>
     </a>
