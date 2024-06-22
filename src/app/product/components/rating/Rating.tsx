d04b2f1aaa2e95758ed98dcbf277ca42e2a43907
zoonyoung <9851248@gmail.com>
2024-06-22 15:50:16 +0900

    fix: defaultValue 추가

--- src/app/product/components/rating/Rating.tsx
+++ src/app/product/components/rating/Rating.tsx
@@ -5,12 +5,12 @@ import cn from "@/utils/classNames";
 import { STAR_ACTIVE_ICON, STAR_ICON } from "@/utils/constant";
 import styles from "./Rating.module.scss";
 
-type RatingProps<T extends FieldValues> = { name: Path<T>; setValue: UseFormSetValue<T> };
+type RatingProps<T extends FieldValues> = { name: Path<T>; setValue: UseFormSetValue<T>; defaultValue?: number };
 const STAR_LIST = new Array(5).fill(0).map((_, index) => ({ id: index }));
 
-export default function Rating<T extends FieldValues>({ name, setValue }: RatingProps<T>) {
-  const [rating, setRating] = useState(0);
-  const [hoverRating, setHoverRating] = useState<number>(0);
+export default function Rating<T extends FieldValues>({ name, defaultValue = 1, setValue }: RatingProps<T>) {
+  const [rating, setRating] = useState(defaultValue - 1);
+  const [hoverRating, setHoverRating] = useState<number>(defaultValue - 1);
 
   const handleClick = (index: number) => {
     setRating(index);
