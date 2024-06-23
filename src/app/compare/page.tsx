93c61853821caf09a7ee7de22751ba8814dd44d5
yubin <yb3143@gmail.com>
2024-06-23 14:03:13 +0900

    [#28] chore: comparePage 로컬 스토리지 수정

--- src/app/compare/page.tsx
+++ src/app/compare/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Product } from "@/app/compare/input/compareProductItem";
 import { LOADING_LARGE_IMAGE } from "@/utils/constant";
 import styles from "./compare.module.scss";
 import ProductSelector from "./input/CompareInput";
+import { getFromLocalStorage, saveToLocalStorage } from "./input/localStorage";
 import CompareTable from "./table/CompareTable";
 
 function ComparePage() {
@@ -14,8 +15,16 @@ function ComparePage() {
     product2: Product | null;
   }>({ product1: null, product2: null });
 
+  useEffect(() => {
+    const savedProduct1 = getFromLocalStorage("selectedProduct1");
+    const savedProduct2 = getFromLocalStorage("selectedProduct2");
+    setSelectedProducts({ product1: savedProduct1, product2: savedProduct2 });
+  }, []);
+
   const handleCompare = (product1: Product | null, product2: Product | null) => {
     setSelectedProducts({ product1, product2 });
+    saveToLocalStorage("selectedProduct1", product1);
+    saveToLocalStorage("selectedProduct2", product2);
   };
 
   return (
