14cbd4f5ed32c043c033430ef0d49baaae67107d
zoonyoung <9851248@gmail.com>
2024-06-24 12:17:42 +0900

    fix: useQuery -> useSuspenseQuery변경(Suspnese컴포넌트 활용을 위해서)

--- src/app/product/components/shopping/Shopping.tsx
+++ src/app/product/components/shopping/Shopping.tsx
@@ -1,23 +1,22 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import CoupangCard from "@/app/product/components/card/CoupangCard";
 import NaverCard from "@/app/product/components/card/NaverCard";
-import ProductSkeleton from "@/app/product/components/skeleton/ProductSkeleton";
 import { fetchShoppingList } from "@/app/product/utils/apis";
 import { CoupangProduct, NaverProduct } from "@/app/product/utils/types";
 import styles from "./Shopping.module.scss";
 
 export default function Shopping({ name }: { name: string }) {
-  const { isPending: coupangIsLoading, data: coupangData } = useQuery<{ items: CoupangProduct[] }>({
+  const { data: coupangData } = useSuspenseQuery<{ items: CoupangProduct[] }>({
     queryKey: ["coupang", name],
-    queryFn: () => fetchShoppingList("coupang", name),
+    queryFn: () => fetchShoppingList("coupang", name) as Promise<{ items: CoupangProduct[] }>,
     staleTime: 30 * 60 * 1000,
     gcTime: 60 * 60 * 1000,
   });
-  const { isPending: naverIsLoading, data: naverData } = useQuery<{ items: NaverProduct[] }>({
+  const { data: naverData } = useSuspenseQuery<{ items: NaverProduct[] }>({
     queryKey: ["naver", name],
-    queryFn: () => fetchShoppingList("naver", name),
+    queryFn: () => fetchShoppingList("naver", name) as Promise<{ items: NaverProduct[] }>,
     staleTime: 30 * 60 * 1000,
     gcTime: 60 * 60 * 1000,
   });
@@ -25,28 +24,20 @@ export default function Shopping({ name }: { name: string }) {
   return (
     <section className={styles.container}>
       <div className={styles.layout}>
-        {coupangIsLoading ? (
-          <ProductSkeleton />
-        ) : (
-          coupangData?.items?.map((product) => (
-            <CoupangCard
-              key={product.link}
-              product={product}
-            />
-          ))
-        )}
+        {coupangData?.items?.map((product) => (
+          <CoupangCard
+            key={product.link}
+            product={product}
+          />
+        ))}
       </div>
       <div className={styles.layout}>
-        {naverIsLoading ? (
-          <ProductSkeleton />
-        ) : (
-          naverData?.items?.map((product) => (
-            <NaverCard
-              key={product.link}
-              product={product}
-            />
-          ))
-        )}
+        {naverData?.items?.map((product) => (
+          <NaverCard
+            key={product.link}
+            product={product}
+          />
+        ))}
       </div>
     </section>
   );
