d43430b0f51d4142719e169c4faf0309577011bc
zoonyoung <9851248@gmail.com>
2024-06-25 13:00:41 +0900

    fix: coupangData 데이터 없을시 빈객체 전달

--- src/app/api/search/coupang/route.ts
+++ src/app/api/search/coupang/route.ts
@@ -5,8 +5,12 @@ import { CoupangProduct } from "@/app/product/utils/types";
 export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl;
   const keyword = searchParams.get("keyword") || "";
-  const response = await fetch(`https://www.coupang.com/np/search/?component=&q=${encodeURIComponent(keyword)}`);
-  const items: CoupangProduct[] = coupangScraper(await response.text()).slice(0, 5);
-
-  return NextResponse.json({ items });
+  try {
+    const response = await fetch(`https://www.coupang.com/np/search/?component=&q=${encodeURIComponent(keyword)}`);
+    const items: CoupangProduct[] = coupangScraper(await response.text()).slice(0, 5);
+    return NextResponse.json({ items });
+  } catch (error) {
+    console.log("data scrapping Error", error);
+    return NextResponse.json({ items: [] });
+  }
 }
