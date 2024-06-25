4d9708fa45e7ee5057b67c5ccc3c2e410945d5d5
zoonyoung <9851248@gmail.com>
2024-06-25 12:19:29 +0900

    add: cors해결을 위한 설정파일 추가

--- next.config.mjs
+++ next.config.mjs
@@ -4,25 +4,5 @@ const nextConfig = {
     domains: ["lh3.googleusercontent.com", "sprint-fe-project.s3.ap-northeast-2.amazonaws.com"],
   },
   compiler: {},
-
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
 };
-
-module.exports = nextConfig;
+export default nextConfig;
