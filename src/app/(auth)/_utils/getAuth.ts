eaad6dc3e80235049998776b23caa33d592c6e67
aoooec <foottable@gmail.com>
2024-06-28 22:53:36 +0900

    feat: getAuth 유틸 함수 생성

---
+++ src/app/(auth)/_utils/getAuth.ts
@@ -0,0 +1,20 @@
+type GetAuthParams = {
+  path: string;
+  body?: { [key: string]: string | number };
+};
+
+const getAuth = async ({ path, body }: GetAuthParams) => {
+  const result = await fetch(`${process.env.BASE_URL}${path}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: body ? JSON.stringify(body) : "",
+  });
+
+  const data = await result.json();
+
+  return { result, data };
+};
+
+export default getAuth;
